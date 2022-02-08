b1e443c8c896f345020a6a047e922498c2bca4e8
Anuraj Kumar <akr801305@gmail.com>
2022-02-08 23:56:27 +0530

    done

--- checkinterger.java
+++ checkinterger.java
@@ -1,6 +1,7 @@
 //Write a Java program to detect whether a number entered by the user is an integer or not.	
 import java.util.Scanner;
-public class checkinteger{
+
+public class checkinterger{
       public static void main(String[] args){
 	Scanner s = new Scanner(System.in);
 	System.out.print("Enter a Number");
